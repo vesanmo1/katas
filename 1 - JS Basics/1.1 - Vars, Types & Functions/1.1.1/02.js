b300f4995d89f91209d382f0d6307c0385733745
vesanmo1 <veronicasanchez.design@gmail.com>
2023-02-09 15:16:09 +0100

    1.1.1. -> 02.js done

--- 1 - JS Basics/1.1 - Vars, Types & Functions/1.1.1/02.js
+++ 1 - JS Basics/1.1 - Vars, Types & Functions/1.1.1/02.js
@@ -3,4 +3,15 @@
 const a = 4;
 a = 5;
 
+//Las declaraciones "const" no se pueden reasignar ni volver a declarar.
+
 console.log(a);
+
+// Una posible solución (si realemente no necesitas que sea una constante):
+
+let b = 4;
+b = 5;
+
+//Las declaraciones "const" no se pueden reasignar ni volver a declarar.
+
+console.log(b);
