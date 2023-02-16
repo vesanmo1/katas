799d1418fcf2dfffddbe3bede7d8e191562f8321
vesanmo1 <veronicasanchez.design@gmail.com>
2023-02-16 13:03:19 +0100

    1.1.3. -> 03 done

--- 1 - JS Basics/1.1 - Vars, Types & Functions/1.1.3/03.js
+++ 1 - JS Basics/1.1 - Vars, Types & Functions/1.1.3/03.js
@@ -1,4 +1,11 @@
 // Crea una función que devuelva el número negativo del parámetro
 
-inverse(4) // Debe mostrar -4
-inverse(2) // Debe mostrar -2
+inverse(4); // Debe mostrar -4
+inverse(2); // Debe mostrar -2
+
+function inverse(num) {
+  return -Math.abs(num);
+}
+
+console.log(inverse(4));
+console.log(inverse(2));
