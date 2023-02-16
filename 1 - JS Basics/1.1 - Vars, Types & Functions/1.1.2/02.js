39c4a79ce9a6ba14209ab3c9c847af51979cdf64
vesanmo1 <veronicasanchez.design@gmail.com>
2023-02-16 12:24:11 +0100

    1.1.2. -> 02 done

--- 1 - JS Basics/1.1 - Vars, Types & Functions/1.1.2/02.js
+++ 1 - JS Basics/1.1 - Vars, Types & Functions/1.1.2/02.js
@@ -1,13 +1,17 @@
-// Crea dos variables nuevas que contengan true o false en base 
-// a si el valor de x1 y x2 es mayor que 5. 
+// Crea dos variables nuevas que contengan true o false en base
+// a si el valor de x1 y x2 es mayor que 5.
 // Utiliza comparaciones para esto con los operadores de igualdad
 // Muestra esos valores por consola
+
 const x1 = 2;
 const x2 = 7;
 
+let MenorIgualQue5 = x1 > 5;
+let MayorQue5 = x2 > 5;
+
 // 👇🏻 Tendrás que almacenar el valor de la comparación aquí modificando estas líneas
-const x1Comparation = false;
-const x2Comparation = false;
+const x1Comparation = MenorIgualQue5;
+const x2Comparation = MayorQue5;
 // 👆🏻
 
 console.log(x1Comparation, x2Comparation);
