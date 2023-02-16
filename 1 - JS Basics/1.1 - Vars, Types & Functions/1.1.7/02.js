a17c333a6dc959d6ecbe23a71e2c043a81bbad9e
vesanmo1 <veronicasanchez.design@gmail.com>
2023-02-16 15:44:15 +0100

    1.1.7 -> 02 done

--- 1 - JS Basics/1.1 - Vars, Types & Functions/1.1.7/02.js
+++ 1 - JS Basics/1.1 - Vars, Types & Functions/1.1.7/02.js
@@ -1,10 +1,12 @@
 // Crea una función que reemplace todas las 'a' de un texto por 'i'
 
 // 👇🏻 Define aquí tu función
-const replaceA = () => {
-
+const replaceA = (text) => {
+  return text.replaceAll("a", "i");
 };
 // 👆🏻
 
-replaceA('hola que tal'); // holi que til
-replaceA('me llamo Carlos'); // me llimo Cirlos
+const text1 = replaceA("hola que tal"); // holi que til
+const text2 = replaceA("me llamo Carlos"); // me llimo Cirlos
+
+console.log(text1, text2);
